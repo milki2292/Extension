bc882847f19e2b40beff2aab90eeffc71f94c740
milki2292 <kamil.szmuc2292@gmail.com>
2020-08-28 12:12:27 +0200

    adding icons

--- src/background.js
+++ src/background.js
@@ -8,15 +8,34 @@ function MyGenericClick(info, tab){
 
 function MyImageClick(info, tab){
     console.log("Cliked image", info, tab)
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
+        "type" : "popup"
+    })
+}
+function MyQuoteClick(info, tab){
+    console.log("Cliked quote", info, tab)
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
+        "type" : "popup"
+    })
 }
 
-chrome.contextMenus.create({
-    "title" : "Share",
-    "contexts" : ["page"],
-    "onclick" : MyGenericClick
-})
+
 chrome.contextMenus.create({
     "title" : "Share image",
     "contexts" : ["image"],
     "onclick" : MyImageClick
+})
+chrome.contextMenus.create({
+    "title" : "Share Quote",
+    "contexts" : ["selection"],
+    "onclick" : MyQuoteClick
+})
+
+
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+    console.log("message", msg)
+    sendResponse({"text": "Received the links"})
 })
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-
-window.onload = function() {
-
-    
-    var videoUrls = window.ytplayer.config.args.loaderUrl
-    console.log("Ex loaded", videoUrls)
-}
+window.onload = function () {
+  var videoUrls = window.ytplayer.config.args.loaderUrl;
+  console.log("Ex loaded", videoUrls);
+};
 console.log("Ex loaded");
 alert("loaded");
 
 var links = document.getElementsByTagName("a");
+var formatted_links = [];
 
+for (let i = 0; i < links.length; i++) {
+  let title = links[i].text;
+  let href = links[i].href;
 
-for(let i = 0; i < links.length; i++){
-    let title = links[i].innerHTML;
-    console.log("link is", title)
+  if (title !== "" && href !== "") {
+    formatted_links.push({ title: title, href: href });
+  }
 }
+
+chrome.runtime.sendMessage(
+  {
+    action: "print_messages",
+    data: formatted_links,
+  },
+  (res) => {
+    console.log(res);
+  }
+);
